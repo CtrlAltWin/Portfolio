e12749a4ee1c87e9f299de5ae4b62d06cfeb9303
Raunak <raunakkumar7245@gmail.com>
2025-02-01 20:33:35 +0530

    Ui improvements

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -38,11 +38,11 @@ const Education = () => {
         backgroundColor: themeColor,
       }}
     >
-      <div className="fixed top-32 sm:top-11 flex sm:flex-col w-screen gap-3 p-1 sm:p-4 sm:gap-6 rounded-lg justify-center items-center mt-24">
+      <div className="fixed top-32 sm:top-12 flex sm:flex-col w-screen gap-3 p-1 sm:p-4 sm:gap-6 rounded-lg justify-center items-center mt-24">
         {educationDetails.map((item, index) => (
           <div
             key={index}
-            className="p-5 border rounded-lg shadow-lg w-[25%] min-w-[105px] h-60 sm:h-auto sm:w-[60%] sm:min-w-[500px] flex flex-col items-center text-center hover:scale-105 transition-transform"
+            className="p-4 sm:p-2 border rounded-lg shadow-lg w-[25%] min-w-[105px] h-60 sm:h-auto sm:w-[60%] sm:min-w-[500px] flex flex-col items-center text-center hover:scale-105 transition-transform"
             style={{
               backgroundColor: cardColor,
             }}
