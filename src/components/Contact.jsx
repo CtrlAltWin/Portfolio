e4b62a2ca6643f48655c7be3041ea88a25f0102e
Raunak <raunakkumar7245@gmail.com>
2025-02-08 09:35:34 +0530

    bug fix - 7

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -34,7 +34,7 @@ const Contact = () => {
               opacity: 1,
             }}
             transition={{
-              duration: 0.2,
+              duration: 0.1,
               ease: "easeInOut",
             }}
             key={index}
